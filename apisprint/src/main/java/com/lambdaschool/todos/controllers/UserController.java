9a96721b05ff0136869405e4d161101ba26fcf30
Justin Lohner <lohner4@gmail.com>
2020-06-12 11:40:39 -0600

    Got user post request working

--- apisprint/src/main/java/com/lambdaschool/todos/controllers/UserController.java
+++ apisprint/src/main/java/com/lambdaschool/todos/controllers/UserController.java
@@ -3,13 +3,15 @@ package com.lambdaschool.todos.controllers;
 import com.lambdaschool.todos.models.User;
 import com.lambdaschool.todos.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -28,5 +30,21 @@ public class UserController {
 	public ResponseEntity<?> getUserById(@PathVariable long userid) {
 		User user = userService.findUserById(userid);
 		return new ResponseEntity<>(user, HttpStatus.OK);
+	}
+
+	@PostMapping(value = "/user", consumes = {"application/json"})
+	public ResponseEntity<?> addNewUser(@Valid @RequestBody User newuser) throws URISyntaxException {
+		newuser.setUserid(0);
+		newuser = userService.save(newuser);
+
+		HttpHeaders responseHeaders = new HttpHeaders();
+		URI newUserURI = ServletUriComponentsBuilder
+							.fromCurrentRequest()
+							.path("/{userid}")
+							.buildAndExpand(newuser.getUserid())
+							.toUri();
+		responseHeaders.setLocation(newUserURI);
+
+		return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
 	}
 }
